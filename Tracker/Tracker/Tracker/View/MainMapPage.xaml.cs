1f04c8fb27a34ad87ff74b5ca2c50db4704e5aaa
pshukla <pshukla@PSHUKLA2L2>
2018-06-12 19:00:11 +0530

    "current position working"

--- Tracker/Tracker/Tracker/View/MainMapPage.xaml.cs
+++ Tracker/Tracker/Tracker/View/MainMapPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tracker.ViewModel;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace Tracker.View
@@ -13,6 +15,25 @@ namespace Tracker.View
 	public partial class MainMapPage : ContentPage
 	{
         public MainMapPageViewModel viewModel;
+
+        private async Task RetrieveLocation()
+        {
+            var location = CrossGeolocator.Current;
+            location.DesiredAccuracy = 20;
+
+            var position = await location.GetPositionAsync(TimeSpan.FromSeconds(10));
+
+            var latitude = position.Latitude;
+            var longitude = position.Longitude;
+
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude), Distance.FromMeters(500)));
+        }
+
+        private async void RetrieveLocation1()
+        {
+            await RetrieveLocation();
+        }
+
         public MainMapPage ()
 		{
 			InitializeComponent ();
@@ -20,6 +41,9 @@ namespace Tracker.View
 
             InitializeComponent();
             this.BindingContext = viewModel;
+
+            RetrieveLocation1();
         }
+
 	}
 }
